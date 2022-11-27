8000487bbc603773703776305b60f92ef0b64c96
bobeenlee <globaldev@naver.com>
2022-11-27 21:24:45 +0900

    feat: egjs 구현 조정

--- libs/features/egjs-animation/src/lib/components/scroll-press-center/scroll-press-center.tsx
+++ libs/features/egjs-animation/src/lib/components/scroll-press-center/scroll-press-center.tsx
@@ -1,7 +1,7 @@
 import Flicking from '@egjs/react-flicking';
 import styles from './scroll-press-center.module.css';
 import '@egjs/react-flicking/dist/flicking.css';
-import { useCallback, useRef } from "react";
+import { useCallback, useRef } from 'react';
 
 interface Item {
   id: string;
@@ -13,37 +13,28 @@ export interface ScrollPressCenterProps {
 }
 
 export function ScrollPressCenter(props: ScrollPressCenterProps) {
-  const { items } = props
-  const flickingRef = useRef<Flicking>(null)
+  const { items } = props;
+  const flickingRef = useRef<Flicking>(null);
+  const isMove = useRef(false);
 
   const onSelected = useCallback((index: number) => {
-    let loading = false
     return async () => {
-      if(loading) {
-        return
+      if (isMove.current) {
+        return;
       }
-      console.log(index)
-      loading = true
+      isMove.current = true;
       await flickingRef.current?.moveTo(index).catch(() => void 0);
-      loading = false
+      isMove.current = false;
     };
   }, []);
 
   return (
-    <Flicking
-      ref={flickingRef}
-      preventClickOnDrag={false}
-      align="center"
-    >
-       {items.map((item, i) => (
-          <div
-            key={item.id}
-            className={styles['item']}
-            onClick={onSelected(i)}
-          >
-            {item.value}{' '}
-          </div>
-        ))}
+    <Flicking ref={flickingRef} preventClickOnDrag={false} align="center">
+      {items.map((item, i) => (
+        <div key={item.id} className={styles['item']} onClick={onSelected(i)}>
+          {item.value}{' '}
+        </div>
+      ))}
     </Flicking>
   );
 }
