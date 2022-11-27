7d8d0f5735703459c4eefed6c6b00d5d38dede55
bobeenlee <globaldev@naver.com>
2022-11-27 18:36:23 +0900

    feat: motion 오동작 조정중

--- libs/features/framer-motion-animation/src/lib/components/scroll-press-center/scroll-press-center.tsx
+++ libs/features/framer-motion-animation/src/lib/components/scroll-press-center/scroll-press-center.tsx
@@ -1,5 +1,5 @@
-import { motion, useScroll } from 'framer-motion';
-import { useCallback, useRef, useState } from 'react';
+import { motion } from 'framer-motion';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import styles from './scroll-press-center.module.css';
 
 interface Item {
@@ -14,38 +14,49 @@ export interface ScrollPressCenterProps {
 export function ScrollPressCenter(props: ScrollPressCenterProps) {
   const { items } = props;
   const ref = useRef<HTMLDivElement | null>(null);
+  
   const innerRef = useRef<HTMLDivElement | null>(null);
+  const [innerWidth, setInnerWidth] = useState<number>(0)
+
   const childrenRefs = useRef<(HTMLDivElement | null)[]>(Array(items.length));
-  const [target, setTarget] = useState<HTMLDivElement | null>(null);
-  const targetRef = useRef<HTMLDivElement | null>(null);
-  console.log(target);
-  const { scrollX } = useScroll({ axis: "x", container: innerRef, target: targetRef, offset: ["center"] })
 
-  console.log(scrollX)
+  useEffect(() => {
+    setInnerWidth(innerRef.current?.scrollWidth ?? 0)
+  }, [items])
 
+  // scroll width, element width, 
   const onSelected = useCallback((index: number) => {
-    return () => {
-      console.log(index);
-      childrenRefs.current[index]?.scrollIntoView({
-        behavior: "smooth",
-        block: "center",
-        inline: "center"
-      })
-      // setTarget(childrenRefs.current[index]);
-      // targetRef.current = childrenRefs.current[index];
+    return (e: any) => {
+      e.preventDefault()
+      console.log(index, childrenRefs.current[index]?.offsetLeft, innerRef.current?.clientLeft);
+      // ref.current?.scrollTo(childrenRefs.current[index]?.offsetLeft ?? 0, 0)
+      // childrenRefs.current[index]?.scrollIntoView({
+      //   behavior: "smooth",
+      //   block: "center",
+      //   inline: "center"
+      // })
     };
   }, []);
 
+  // FIXME https://github.com/framer/motion/issues/185
+  // FIXME https://github.com/framer/motion/issues/314
   return (
     <motion.div ref={ref} className={styles['wrap']}>
       <motion.div
         ref={innerRef}
         style={{
-          width: 6000, // items.length * width
+          width: innerWidth,
         }}
         className={styles['inner']}
-        drag={'x'}
+        drag={(innerWidth > 0) ? 'x' : false}
         dragConstraints={ref}
+      //   onDrag={(event, pan) => {
+      //     (pan as any).preventDefault() // have this prevent the animation
+      //     console.log(ref.current?.scrollLeft, pan)
+      //     if(ref.current) {
+      //       ref.current.scrollLeft += -pan.offset.x // something like that
+      //     }
+      //  }}
       >
         {items.map((item, i) => (
           <motion.div
